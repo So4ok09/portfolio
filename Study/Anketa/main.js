9ddfc420f259a9d3f1ef3bdc74047a8719c44f29
So4ok09 <danila.sockov@yandex.ru>
2020-11-18 16:49:46 +0300

    Add files via upload

---
+++ Study/Anketa/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Введите имя");
+let lastName =  prompt("Введите фамилию");
+let age = prompt("Введите возраст"); 
+
+console.log("Пользователь: " + firstName + " " +  lastName);
+console.log("Возраст: " + age);
